30f723b98ab302269804bdb528525aa2ad413928
iamalibabaei <alibabaei037@gmail.com>
2018-12-20 00:08:00 +0330

    گربه تقریبا اوکی شد. برای ذخیره‌ی محصولات باید هش‌مپ انبار تغییر پیدا کند

--- src/models/animal/Cat.java
+++ src/models/animal/Cat.java
@@ -1,22 +1,36 @@
 package models.animal;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import models.Entity;
+import models.Item;
 import models.interfaces.Buyable;
+import models.map.Cell;
+import models.map.Map;
 
 public class Cat extends Animal implements Buyable
 {
-    public static final int BUY_COST = 0, SELL_MONEY = 0, OCCUPATION_SPACE = 0;
+    public static final int BUY_COST = 2500, SELL_MONEY = 0, OCCUPATION_SPACE = 0;
     private static int level;
 
-    public Cat()
+    public Cat(Map map)
     {
-        super();
+        super(map);
     }
 
     @Override
     public void setTarget()
     {
+        super.target = null;
 
+        for (Cell[] cells: super.map.getCells()) {
+            for (Cell cell: cells) {
+                for (Entity entity : cell.getEntities()) {
+                    if (entity instanceof Item){
+                        super.target = entity;
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -28,7 +42,10 @@ public class Cat extends Animal implements Buyable
     @Override
     public void collide(Entity entity)
     {
-
+        if (entity instanceof Item){
+            super.map.getCell(entity.getX(), entity.getY()).getStorables();
+            super.map.getCell(entity.getX(), entity.getY()).getEntities().remove(entity);
+        }
     }
 
     @Override
