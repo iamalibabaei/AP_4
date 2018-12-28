d3ad3bd9ca55c583ec385fc6c4f1f309e70a1554
Parsa Salavati <parsa.salavati@gmail.com>
2018-12-28 10:40:40 +0330

    Upgradable interface completed Added an interface Countdown: for classes which do something over time (like workshops)

---
+++ src/models/interfaces/Countdown.java
@@ -0,0 +1,6 @@
+package models.interfaces;
+
+public interface Countdown
+{
+    void countdown();
+}
--- src/models/interfaces/Upgradable.java
+++ src/models/interfaces/Upgradable.java
@@ -1,10 +1,10 @@
 package models.interfaces;
 
-import models.exceptions.MaxlevelException;
+import models.exceptions.AlreadyAtMaxLevelException;
 
 public interface Upgradable
 {
-
-    void upgrade() throws MaxlevelException;
+    void upgrade() throws AlreadyAtMaxLevelException;
+    int getUpgradeCost() throws AlreadyAtMaxLevelException;
 
 }
