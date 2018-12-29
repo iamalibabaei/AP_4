f701adf765afb574a30364de801b5e8f720a4789
iamalibabaei <alibabaei037@gmail.com>
2018-12-29 23:38:29 +0330

    -_____-

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,7 +1,10 @@
 package models;
 
+import models.animal.*;
+import models.map.Cell;
 import models.map.Map;
 import models.transportation.Buyer;
+import models.transportation.Seller;
 import models.workshop.Workshop;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ public class Game
     private Warehouse warehouse;
     private Well well;
     private Buyer truck;
+    private Seller helicopter;
     private ArrayList<Workshop> workshops;
     private Mission mission;
     private int wildAnimalsInMap;
@@ -32,12 +36,63 @@ public class Game
         return money;
     }
 
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+
+    public void addDomesticAnimal(DomesticAnimal animal){
+        if (money >= animal.getBuyCost()) {
+            map.getCell(animal.x, animal.y).addEntity(animal);
+            money -= animal.getBuyCost();
+        }
+    }
+
+    public void addCat(Cat cat){
+        if (money >= cat.getBuyCost()) {
+            map.getCell(cat.x, cat.y).addEntity(cat);
+            money -= cat.getBuyCost();
+        }
+    }
+
+    public void addDog(Dog dog){
+        if (money >= dog.getBuyCost()) {
+            map.getCell(dog.x, dog.y).addEntity(dog);
+            money -= dog.getBuyCost();
+        }
+    }
+
     private Game()
     {
         map = new Map();
         warehouse = new Warehouse();
         well = new Well();
         workshops = new ArrayList<>();
+        truck = new Buyer(map);
+        helicopter = new Seller();
+        mission = new Mission(ourInstance);
+
+
+        map.handleCollisions();
+
+        well.nextTurn();
+        truck.nextTurn();
+        helicopter.nextTurn();
+        for (Workshop workshop : workshops) {
+            workshop.nextTurn();
+        }
+        for (Cell[] cells: map.getCells()) {
+            for (Cell cell: cells) {
+                for (Entity entity : cell.getEntities()) {
+                    if (entity instanceof Animal){
+                        ((Animal) entity).nextTurn();
+                    }
+
+                }
+
+            }
+
+        }
     }
 
 
