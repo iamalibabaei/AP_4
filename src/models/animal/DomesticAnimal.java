5e7295641bfbdaeb8c8b213091e22a9515f9264c
Parsa Salavati <parsa.salavati@gmail.com>
2018-12-29 19:55:57 +0330

    Domestic animal completed.

--- src/models/animal/DomesticAnimal.java
+++ src/models/animal/DomesticAnimal.java
@@ -9,11 +9,43 @@ import models.map.Map;
 
 public class DomesticAnimal extends Animal implements Buyable, Countdown
 {
-    public static final int MAX_SATURATED_RATE = 20;
-    private int saturatedRate = MAX_SATURATED_RATE / 2;
+    public static final int MAX_SATURATION_RATE = 20;
+    private int saturationRate = MAX_SATURATION_RATE / 2;
     private boolean isHungry = true;
 
     private Type type;
+
+    public DomesticAnimal(int x, int y, Map map, Type type)
+    {
+        super(x, y, map);
+        this.type = type;
+    }
+
+    @Override
+    public void countdown()
+    {
+        if (saturationRate <= 0)
+        {
+            this.die();
+        }
+        saturationRate--;
+        if (!isHungry && saturationRate <= MAX_SATURATION_RATE / 2)
+        {
+            produce();
+            isHungry = true;
+        }
+    }
+
+    private void produce()
+    {
+        map.getCell(x, y).addEntity(new Item(x, y, type.PRODUCT));
+    }
+
+    @Override
+    public int getBuyCost()
+    {
+        return type.BUY_COST;
+    }
 
     public enum Type
     {
@@ -24,54 +56,29 @@ public class DomesticAnimal extends Animal implements Buyable, Countdown
         public final int BUY_COST;
         public final Item.Type PRODUCT;
 
-        Type(int BUY_COST, Item.Type item) {
+        Type(int BUY_COST, Item.Type item)
+        {
             this.BUY_COST = BUY_COST;
             this.PRODUCT = item;
         }
-    }
-
-     public DomesticAnimal(int x, int y, Map map, Type type)
-    {
-        super(x, y, map);
-        this.type = type;
-    }
-
-    @Override
-    public void countdown() {
-
-        if (!isHungry){
-            saturatedRate--;
-            if (saturatedRate <= MAX_SATURATED_RATE / 2){
-                produce();
-                isHungry = true;
-            }
-        }else {
-            if (target == null){
-                saturatedRate--;
-            }
-            if (saturatedRate <= 0){
-                this.die();
-            }
-        }
-    }
-
-    private void produce()
-    {
-
-        map.getCell(x, y).addEntity(new Item(x, y, type.PRODUCT));
     }
 
     @Override
     public void setTarget()
     {
         target = null;
-        if (isHungry){
+        if (isHungry)
+        {
             int dist = 1000;
-            for (Cell[] cells : map.getCells()){
-                for (Cell cell : cells){
-                    if (cell.getGrass() > 0){
+            for (Cell[] cells : map.getCells())
+            {
+                for (Cell cell : cells)
+                {
+                    if (cell.getGrass() > 0)
+                    {
                         int dist1 = Math.abs(this.x - cell.getX()) + Math.abs(this.y - cell.getY());
-                        if (dist1 < dist) {
+                        if (dist1 < dist)
+                        {
                             dist = dist1;
                             target.setX(cell.getX());
                             target.setY(cell.getY());
@@ -85,11 +92,14 @@ public class DomesticAnimal extends Animal implements Buyable, Countdown
     @Override
     public void collide(Entity entity)
     {
-        if (isHungry) {
-            if (map.getCell(entity.getX(), entity.getY()).getGrass() > 0){
+        if (isHungry)
+        {
+            if (map.getCell(entity.getX(), entity.getY()).getGrass() > 0)
+            {
                 map.getCell(entity.getX(), entity.getY()).eatGrass();
-                saturatedRate++;
-                if (saturatedRate >= MAX_SATURATED_RATE){
+                saturationRate++;
+                if (saturationRate >= MAX_SATURATION_RATE)
+                {
                     isHungry = false;
                 }
             }
@@ -97,10 +107,5 @@ public class DomesticAnimal extends Animal implements Buyable, Countdown
         setTarget();
     }
 
-    @Override
-    public int getBuyCost()
-    {
-        return type.BUY_COST;
-    }
 
 }
