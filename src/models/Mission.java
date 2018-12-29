84efae206c023e49bcc018a550fe1d4070d4864f
alipppp <alipourghasemi78@gmail.com>
2018-12-29 21:14:02 +0330

    misson

--- src/models/Mission.java
+++ src/models/Mission.java
@@ -1,20 +1,43 @@
 package models;
 
+import models.animal.DomesticAnimal;
+
 import java.util.HashMap;
 
 public class Mission
 {
     private int money;
-    private HashMap<Entity, Integer> objectives;
+    HashMap<DomesticAnimal.Type, Integer> animalObjectives;
+    HashMap<Item.Type, Integer> ItemObjective;
 
-    public HashMap<Entity, Integer> getObjectives()
-    {
-        return objectives;
+    public Mission(int money, HashMap<DomesticAnimal.Type, Integer> animalObjectives,
+                   HashMap<Item.Type, Integer> itemObjective) {
+        this.money = money;
+        this.animalObjectives = animalObjectives;
+        ItemObjective = itemObjective;
     }
 
-    public boolean isAccomplished(int money, HashMap<Entity, Integer> currentState)
+    public boolean isAccomplished(int money, HashMap<DomesticAnimal.Type, Integer> animalCurrentState,
+                                  HashMap<Item.Type, Integer> ItemCurentState )
     {
-        return money > this.money && currentState.equals(objectives);
-    }
+        if (this.money > money) {
+            return false;
+        }
+        for (DomesticAnimal.Type type : animalCurrentState.keySet()) {
+            if (animalObjectives.containsKey(type)) {
+                animalObjectives.get(type) > animalCurrentState.get(type) {
+                    return false;
+                }
+            }
+        }
 
+        for (Item.Type type : ItemCurentState.keySet()) {
+            if (ItemObjective.containsKey(type)) {
+                if (ItemObjective.get(type) > ItemCurentState.get(type)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
