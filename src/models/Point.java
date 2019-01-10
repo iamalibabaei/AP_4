914d538543dffd8a59930ab63c6121f2799fbb63
iamalibabaei <alibabaei037@gmail.com>
2019-01-10 13:15:54 +0330

    collidswith added

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,45 +1,48 @@
 package models;
 
-public class Point
-{
+public class Point {
     private double x, y;
 
-    public Point(double x, double y)
-    {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double getX()
-    {
+    public double getX() {
         return x;
     }
 
-    public void setX(double x)
-    {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public double getY()
-    {
+    public double getY() {
         return y;
     }
 
-    public void setY(double y)
-    {
+    public void setY(double y) {
         this.y = y;
     }
 
-    public Point normalize(Point point){
+    public Point normalize(Point point) {
         double length = Math.sqrt(point.getX() * point.getX() + point.getY() * point.getY());
         point.setX(point.getX() / length);
         point.setY(point.getY() / length);
         return point;
     }
 
-    public double distance(Point point1){
+    public double distance(Point point1) {
         double distance = Math.sqrt(Math.pow(point1.getX() - this.getX(), 2) + Math.pow(point1.getY() - this.getY(), 2));
         return distance;
+    }
+
+    public boolean collidesWith(Point point) {
+
+        if (this.distance(point) <= Game.RADIUS) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
