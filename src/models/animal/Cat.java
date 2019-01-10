90a148323d3b53dbe178ff09fd1f1438c4c9b3b9
iamalibabaei <alibabaei037@gmail.com>
2019-01-10 13:13:52 +0330

    Class constructor and setTarget and collied methods changed because of map change

--- src/models/animal/Cat.java
+++ src/models/animal/Cat.java
@@ -2,106 +2,87 @@ package models.animal;
 
 import models.Entity;
 import models.Item;
+import models.Map;
 import models.Warehouse;
 import models.exceptions.AlreadyAtMaxLevelException;
 import models.interfaces.Buyable;
 import models.interfaces.Upgradable;
-import models.map.Cell;
-import models.map.Map;
+
 import java.util.HashMap;
 
 // todo cat upgrade
 
-public class Cat extends Animal implements Buyable, Upgradable
-{
+public class Cat extends Animal implements Buyable, Upgradable {
     private static final int BUY_COST = 2500, MAX_LEVEL = 1, UPGRADE_COST = 200;
     private static int level;
     private Warehouse warehouse;
 
-    public Cat(int x, int y, Animal.Type type, Map map, Warehouse warehouse)
-    {
+    public Cat(double x, double y, Animal.Type type, Map map, Warehouse warehouse) {
         super(x, y, type, map);
         this.warehouse = warehouse;
     }
 
     @Override
-    public void setTarget()
-    {
+    public void setTarget() {
         super.target = null;
 
-        if (level == 0){
-            for (Cell[] cells: map.getCells()) {
-                for (Cell cell: cells) {
-                    for (Entity entity : cell.getEntities()) {
-                        if (entity instanceof Item){
-                            target = entity.getPoint();
-                            return;
-                        }
-                    }
-                }
+        if (level == 0) {
+            for (Item item : map.getItems()) {
+                target.setX(item.getCoordinates().getX());
+                target.setY(item.getCoordinates().getY());
+                return;
+
             }
-        }
-        else {
-            int dist = 1000;
-            for (Cell[] cells: map.getCells()) {
-                for (Cell cell: cells) {
-                    for (Entity entity : cell.getEntities()) {
-                        if (entity instanceof Item){
-                            int dist1 = Math.abs(this.getX() - target.getX()) + Math.abs(this.getY() - target.getY());
-                            if (dist1 < dist) {
-                                dist = dist1;
-                                target = entity.getPoint();
-                            }
-                        }
-                    }
+        } else {
+            double dist = 1000;
+            for (Item item : map.getItems()) {
+                double dist1 = this.getCoordinates().distance(item.getCoordinates());
+                if (dist1 < dist) {
+                    dist = dist1;
+                    target.setX(item.getCoordinates().getX());
+                    target.setY(item.getCoordinates().getY());
                 }
             }
         }
     }
 
     @Override
-    public void collide(Entity entity)
-    {
+    public void collide(Entity entity) {
         HashMap<Item.Type, Integer> items = new HashMap<>();
 
-        for (Entity entity1 : map.getCell(this.getX(), this.getY()).getEntities()) {
-            if (entity1 instanceof Item) {
-                if (items.containsKey(((Item) entity1).getType())){
-                    int num = items.get(((Item) entity1).getType()) + 1;
-                    items.put(((Item) entity1).getType(), num);
-                }
-                else {
-                    items.put(((Item) entity1).getType(), 1);
-                }
-                entity1.die();
+        for (Item item : map.getItems()) {
+            if (items.containsKey(item)) {
+                int num = items.get(item) + 1;
+                items.put(item.getType(), num);
+            } else {
+                items.put(item.getType(), 1);
             }
+            item.die();
         }
 
         items = warehouse.store(items);
 
         for (Item.Type item : items.keySet()) {
             for (int i = 0; i < items.get(item); i++) {
-                map.addToMap(new Item(this.getX(), this.getY(), item));
+                map.addToMap(new Item(this.getCoordinates().getX(), this.getCoordinates().getY(), item));
             }
         }
     }
 
     @Override
-    public int getBuyCost()
-    {
+    public int getBuyCost() {
         return BUY_COST;
     }
 
     @Override
-    public void upgrade() throws AlreadyAtMaxLevelException
-    {
+    public void upgrade() throws AlreadyAtMaxLevelException {
         if (level == MAX_LEVEL)
             throw new AlreadyAtMaxLevelException();
         level++;
     }
+
     @Override
-    public int getUpgradeCost() throws AlreadyAtMaxLevelException
-    {
+    public int getUpgradeCost() throws AlreadyAtMaxLevelException {
         if (level == MAX_LEVEL)
             throw new AlreadyAtMaxLevelException();
         return UPGRADE_COST;
