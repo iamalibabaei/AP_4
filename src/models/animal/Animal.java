fc43f33b36222ae368baee3920bd9b04ad599f3a
iamalibabaei <alibabaei037@gmail.com>
2019-01-10 13:12:03 +0330

    direction and speed added, move method changed because of changes in map, Class constructor changed

--- src/models/animal/Animal.java
+++ src/models/animal/Animal.java
@@ -1,76 +1,62 @@
 package models.animal;
 
 import models.Entity;
+import models.Map;
 import models.Point;
 import models.interfaces.Time;
-import models.map.Map;
 
 import java.util.Random;
 
-public abstract class Animal extends Entity implements Time
-{
-    Point target;
-    protected Type type;
+public abstract class Animal extends Entity implements Time {
+    Point target = new Point(-1, -1);
+    public final Type type;
     protected Map map;
+    private static int speed = 5;
+    Point direction;
 
-    public enum Type
-    {
+    public enum Type {
         CAT("cat"), DOG("dog"), LION("lion"), BEAR("bear"), SHEEP("sheep"), HEN("hen"), COW("cow");
 
         public String NAME;
 
-        Type(String NAME)
-        {
+        Type(String NAME) {
             this.NAME = NAME;
         }
     }
 
-    Animal(int x, int y, Type type, Map map)
-    {
+    Animal(double x, double y, Type type, Map map) {
         super(x, y);
         target = null;
         this.map = map;
         this.type = type;
+        direction = new Point(0, 0);
     }
+
 
     public void setTarget() // default move is random
     {
-        target = null;
+        Random random = new Random();
+        target.setX(random.nextDouble() * Map.WIDTH);
+        target.setY(random.nextDouble() * Map.WIDTH);
     }
+
     public abstract void collide(Entity entity);
 
     //if (target == null) randomWalk
-    public void move()
-    {
-        if (target == null){
-            Random random = new Random();
-            this.setX(this.getX()  + (random.nextInt(3 ) - 1));
-            this.setY(this.getY()  + (random.nextInt(3 ) - 1));
+    public void move() {
 
-            if (this.getX() < 0) {this.setX(this.getX() + random.nextInt(2) + 1); }
-            if (this.getY() < 0) {this.setY(this.getY() + random.nextInt(2) + 1); }
-            if (this.getX() > map.WIDTH) { this.setX(this.getX() - random.nextInt(2) + 1); }
-            if (this.getY() > map.HEIGHT) {this.setY(this.getY() - random.nextInt(2) + 1); }
-        }
-        else {
-            if (this.getX()  < target.getX()){
-                this.setX(this.getX() + 1);
-            }
-            else if (this.getX()  > target.getX()){
-                this.setX(this.getX() - 1);
-            }
-            if (this.getY() < target.getY()){
-                this.setY(this.getY() + 1);
-            }
-            else if (this.getY() > target.getY()){
-                this.setY(this.getY() - 1);
-            }
-        }
+        direction.setX(target.getX() - this.getCoordinates().getX());
+        direction.setX(target.getY() - this.getCoordinates().getY());
+        direction = direction.normalize(direction);
+
+        this.getCoordinates().setX(this.getCoordinates().getX() + direction.getX() * Animal.speed);
+        this.getCoordinates().setY(this.getCoordinates().getY() + direction.getY() * Animal.speed);
+
     }
 
     @Override
-    public void nextTurn(){
-            setTarget();
+    public void nextTurn() {
+        setTarget();
         move();
     }
 
