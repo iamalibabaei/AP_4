6b5393bedce19aef3ad8668fbe3ab18c99e4bd48
Parsa Salavati <parsa.salavati@gmail.com>
2019-01-28 16:05:12 +0330

    added constants for addressing

---
+++ src/controller/AddressConstants.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class AddressConstants
+{
+    public static final String RESOURCES_ROOT = "res/";
+    public static final String GRAPHICS_ROOT = "Textures/";
+    public static final String MISSIONS_ROOT = "missions/";
+}
